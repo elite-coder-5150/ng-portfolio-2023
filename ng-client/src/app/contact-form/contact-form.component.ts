7f1a2390396fb1d60828bca69826f205044b1ca0
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-11-01 11:07:45 -0500

    defined the onSubmit method

--- ng-client/src/app/contact-form/contact-form.component.ts
+++ ng-client/src/app/contact-form/contact-form.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
-  selector: 'app-contact-form',
+  selector: 'ng-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent {
+  contactForm?: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.contactForm = this.fb.group({
+      'name': ['', [
+        Validators.required, 
+        Validators.minLength(4), 
+        Validators.maxLength(30)]],
+      'email': ['', [
+        Validators.required, 
+        Validators.minLength(4), 
+        Validators.maxLength(30),
+        Validators.email]],
+      'subject': ['', [
+        Validators.required, 
+        Validators.minLength(15),
+        Validators.maxLength(255)]],
+      'message': ['', [
+        Validators.required, 
+        Validators.minLength(  15 ), 
+        Validators.maxLength(500)]], 
+    })
+  }
+
+  onSubmit() {
+    if (this.contactForm?.valid) {
+      console.log(this.contactForm.value);
+    }
+  }
 }
