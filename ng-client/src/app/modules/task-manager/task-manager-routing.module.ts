8e0e59bb179500d0a1207f499ffcfdc48abe644c
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-11-13 08:15:11 -0600

    defined the new-task, edit-task, and task-list routes

--- ng-client/src/app/modules/task-manager/task-manager-routing.module.ts
+++ ng-client/src/app/modules/task-manager/task-manager-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NewTaskFormComponent } from './new-task-form/new-task-form.component'
+import { TaskListComponent } from './task-list/task-list.component';
+import { EditTaskComponent } from './edit-task/edit-task.component';
 const routes: Routes = [
   {
     path: 'new-task',
     component: NewTaskFormComponent
+  },
+  {
+    path: 'task-list',
+    component: TaskListComponent
+  },
+  {
+    path: 'task/:taskId',
+    component: EditTaskComponent
   }
 ];
 
