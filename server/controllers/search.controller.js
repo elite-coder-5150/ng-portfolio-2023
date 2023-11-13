6b43fb3b410681b957edf716a523cfad1d727a43
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-11-13 08:21:16 -0600

    defined the searchComponentCategory controller

---
+++ server/controllers/search.controller.js
@@ -0,0 +1,51 @@
+import { db } from '../server.js'
+
+/**
+ * 
+ * 
+ * @param {*} req  - the request object
+ * @param {*} res - the response object
+ */
+export const searchUsers = async (req, res) => {
+    const { value } = req.params;
+
+    try {
+        const user_sql = /* sql */ `
+            select u_id, u_name 
+            from users 
+            where u_name 
+            ilike %1 order by id limit 8
+        `;
+        const userResults = await db.query(user_sql, [`%${value}%`]);
+
+        if (userResults.length == 0) {
+            res.status(404).send({error: true, message: 'User not found'});
+        } else {
+            res.status(200).send({userResults});
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export const searchComponent = async (req, res) => {
+    const { category } = req.params;
+
+    try {
+        const sql = /* sql */ `
+            select category from components where category=?
+        `;
+
+        const result = await db.query(sql, [category], (err, result) => {
+            if (err) {
+                throw err;
+            } else if (result.affectedRows >= 1) {
+                res.status(200).send(result);
+            } else {
+                res.status(404).send({error: true, message: result.message});
+            }
+        });
+    } catch (err) {
+        console.error(err);
+    }
+};
