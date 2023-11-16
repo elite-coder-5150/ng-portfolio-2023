558d943777d13c70bcbfcd276a5fe038e9bb3de8
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-11-16 13:32:53 -0600

    defined the unit test for the chat component

--- ng-client/src/app/modules/social/services/chat.service.spec.ts
+++ ng-client/src/app/modules/social/services/chat.service.spec.ts
@@ -4,23 +4,43 @@ import { ChatService } from './chat.service';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 import { cold } from 'jasmine-marbles'
 describe('ChatService', () => {
-  let service: ChatService;
+  let chat: ChatService;
   let mockSocket: WebSocketSubject<any>;
-  
+
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ChatService);
+    chat = TestBed.inject(ChatService);
 
     mockSocket = webSocket('');
 
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(chat).toBeTruthy();
   });
 
   it('should send a message', () => {
     const msg = 'hello world';
     const spy = spyOn(mockSocket, 'next')
+
+    chat.sendMsg(msg);
+    expect(spy).toHaveBeenCalledWith({ type: 'message', content: msg });
+  });
+
+  it ('should receive a message', () => {
+    const msg = 'Received message';
+    const expected = cold('(a|)', { a: { type: 'message', content: msg }});
+
+    chat.receiveMsg().subscribe((result) => {
+      expect(result).toEqual({ type: 'message', content: msg})
+    });
+
+    mockSocket.next({ type: 'message', content: msg });
+    expect(chat.receiveMsg()).toBeObservable(expected);
+  })
+
+  it('should return the socket', () => {
+    const socket = chat.getSocket();
+    expect(socket).toBe(mockSocket);
   })
 });
