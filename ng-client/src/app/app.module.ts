132fbe2aa990a3584bd050fb77c3a3f50aa210a5
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-11-16 13:42:43 -0600

    imported header and footer components

--- ng-client/src/app/app.module.ts
+++ ng-client/src/app/app.module.ts
@@ -4,24 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SocialModule } from './modules/social/social.module';
-import { TaskManagerModule } from './modules/task-manager/task-manager.module';
+
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { NewTaskFormComponent } from './modules/task-manager/new-task-form/new-task-form.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     ContactFormComponent,
-    NewTaskFormComponent
+    NewTaskFormComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SocialModule,
     ReactiveFormsModule,
-    TaskManagerModule,
-  
   ],
   providers: [],
   bootstrap: [AppComponent]
