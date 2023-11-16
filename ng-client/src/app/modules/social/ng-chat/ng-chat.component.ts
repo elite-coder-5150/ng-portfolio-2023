748033e313a2d9200083cb721b44a46d401d0e12
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-11-16 13:40:14 -0600

    defined the unsubscribe method

--- ng-client/src/app/modules/social/ng-chat/ng-chat.component.ts
+++ ng-client/src/app/modules/social/ng-chat/ng-chat.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ChatService } from '../services/chat.service';
 @Component({
   selector: 'app-ng-chat',
   templateUrl: './ng-chat.component.html',
   styleUrls: ['./ng-chat.component.scss']
 })
-export class NgChatComponent {
+export class NgChatComponent implements OnDestroy, OnInit{
+  messages: string[] = [];
+  newMsg: string = '';
 
+  private subscription: Subscription = new Subscription();
+
+  constructor(private chat: ChatService) {}
+
+  ngOnInit(): void {
+    this.subscription = this.chat.receiveMsg().subscribe((msg) => {
+      this.messages.push(msg.content);
+    })
+  }
+
+  ngOnDestroy(): void {
+      this.subscription.unsubscribe()
+  }
+
+  sendMsg(): void {
+    if (this.newMsg.trim() !== '') {
+      this.chat.sendMsg(this.newMsg);
+      this.newMsg = '';
+    }
+  }
 }
