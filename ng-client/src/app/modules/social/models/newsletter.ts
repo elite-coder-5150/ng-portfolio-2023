2d3619bd3eb5105af9d72002a3c97a9b960855e8
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-11-28 15:41:39 -0600

    defined the newsletter interface

---
+++ ng-client/src/app/modules/social/models/newsletter.ts
@@ -0,0 +1,11 @@
+export enum Status {
+    active,
+    inactive
+}
+export interface Newsletter {
+    newsletter_id: number;
+    f_name: string;
+    email: string;
+    status: Status;
+    created_at: Date
+}
