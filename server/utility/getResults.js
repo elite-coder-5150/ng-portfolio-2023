6351bf64fc6d0f65752b881a2e23b1d8d8e8968d
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-11-29 11:18:58 -0600

    refactored the getResults function

---
+++ server/utility/getResults.js
@@ -0,0 +1,11 @@
+export const getResults = (sql, values) => {
+    return new Promise((resolve, reject) => {
+        db.query(sql, values, (err, results) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });
+    })
+}
