ac42529f6102c017d0db9c39ef1eae635f7c7d5e
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-12-07 09:24:39 -0600

    defined the get all components route handler

--- server/controllers/component.controller.js
+++ server/controllers/component.controller.js
@@ -3,19 +3,32 @@ import { getResults } from "../utility/getResults";
 
 
 
-export const getAllComponentsFromUser = async (req, res) => {
+export const getAllComponents = async (req, res) => {
     try {
-        const { userId } = req.params;
+        const sql = /* sql */`
+            select * from components
+        `;
 
-        if (!userId) {
-            return res.status(400).send({
+        const results = await getResults(sql);
+
+        if (results.length === 0) {
+            return res.status(404).send({
                 success: false,
-                message: 'user id is requried'
+                message: 'no components found'
             });
         }
 
+        return res.status(200).send({
+            message: 'successfully retrieved components',
+            data: results
+        })
         
     } catch (error) {
         console.error(error);
+
+        return res.status(500).send({
+            success: false,
+            message: 'internal server error',
+        });
     }
 }
