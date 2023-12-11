0337da345834c9e41b817b8ca13a49e1cacea0c5
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-12-11 10:48:29 -0600

    logic for the button, currently empty, presentation component

---
+++ ng-client/src/app/buttons/sm-secondary/sm-secondary.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sm-secondary',
+  templateUrl: './sm-secondary.component.html',
+  styleUrls: ['./sm-secondary.component.scss']
+})
+export class SmSecondaryComponent {
+
+}
