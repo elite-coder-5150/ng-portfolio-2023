a5d34f13431774b3ecad691b736a7efe66a41c72
elite-coder-5150 <parkhousedarrell7@gmail.com>
2023-12-11 06:40:21 -0600

    imported reactive forms module

--- ng-client/src/app/app.module.ts
+++ ng-client/src/app/app.module.ts
@@ -6,23 +6,28 @@ import { AppComponent } from './app.component';
 import { SocialModule } from './modules/social/social.module';
 
 import { ContactFormComponent } from './contact-form/contact-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { NewTaskFormComponent } from './modules/task-manager/new-task-form/new-task-form.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { CustomCheckboxComponent } from './custom-checkbox/custom-checkbox.component';
+import { SmPrimaryComponent } from './buttons/sm-primary/sm-primary.component';
 @NgModule({
   declarations: [
     AppComponent,
     ContactFormComponent,
     NewTaskFormComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CustomCheckboxComponent,
+    SmPrimaryComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SocialModule,
+    FormsModule,
     ReactiveFormsModule,
   ],
   providers: [],
